ac897476918dffc90e423f9c72aa96f0d7bb22d2
kabita <kabita.texas@gmail.com>
2023-09-11 14:53:12 -0500

    chore: added map in expenses.js

--- src/component/Expenses.js
+++ src/component/Expenses.js
@@ -16,27 +16,9 @@ function Expenses(props) {
         <div>
             <Card className='expenses'>
                 <ExpensesFilter onChangeDateFilter = {filterChangeHandler} year = {filteredYear} />
-                <p>Data for 2019, 2021, 2022 are hidden currently</p>
-                <ExpenseItem
-                    title={props.expenseItem[0].title}
-                    amount={props.expenseItem[0].amount}
-                    date={props.expenseItem[0].date}
-                />
-                <ExpenseItem
-                    title={props.expenseItem[1].title}
-                    amount={props.expenseItem[1].amount}
-                    date={props.expenseItem[1].date}
-                />
-                <ExpenseItem
-                    title={props.expenseItem[2].title}
-                    amount={props.expenseItem[2].amount}
-                    date={props.expenseItem[2].date}
-                />
-                <ExpenseItem
-                    title={props.expenseItem[3].title}
-                    amount={props.expenseItem[3].amount}
-                    date={props.expenseItem[3].date}
-                />
+                {props.expenseItem.map((e) => (
+                    <ExpenseItem title= {e.title}  amount = {e.amount} date = {e.date}/>
+                ))}
             </Card>
         </div>
     )
